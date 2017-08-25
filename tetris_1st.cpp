3393b1891cf0a22e2536e0640ea9e8d12021f62a
jisubshin <https://github.com/jisubshin/tetris_1st>
2017-08-25 11:56:04 +0900

    first commit

--- tetris_1st.cpp
+++ tetris_1st.cpp
@@ -6,21 +6,19 @@ using namespace std;
 
 void gotoxy(int x, int y)
 {
-	COORD Cur;
-	Cur.X = x;
-	Cur.Y = y;
+	COORD Cur = { x, y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
 }
 
 void spin(char(*block)[4]){
 	char tmp[4][4] = { 0 };
 	for (int i = 0; i < 4; i++)
-		for (int j = 0; j < 4; j++)
-			tmp[j][3 - i] = block[i][j];
+	for (int j = 0; j < 4; j++)
+		tmp[j][3 - i] = block[i][j];
 
 	for (int i = 0; i < 4; i++)
-		for (int j = 0; j < 4; j++)
-			block[i][j] = tmp[i][j];
+	for (int j = 0; j < 4; j++)
+		block[i][j] = tmp[i][j];
 }
 
 void change_block(char(*block)[4], char shape){
@@ -169,18 +167,17 @@ void draw(char(*block)[4], int x, int y){
 	system("cls");
 	for (int i = 0; i < 4; i++){
 		for (int j = 0; j < 4; j++){
-			gotoxy(x + 2*j, y + i);
+			gotoxy(x + 2 * j, y + i);
 			if (block[i][j] == 1)
 				printf("бс");
 			else
 				printf("бр");
 		}
-		printf("\n");
 	}
 }
 
 void main(){
-	int x=5, y=5;
+	int x = 5, y = 5;
 	char block_status = 'o';
 	char block[4][4] = { 0 };
 	change_block(block, block_status);
